df326abc519e8fcf2bf7db925c5bb42b3a4dca8a
Tommy Johnsen <7oiden@gmail.com>
2024-09-25 18:45:40 +0200

    footer added

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,10 +2,35 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <div className="footer__item"></div>
-        <div className="footer__item"></div>
-        <div className="footer__item"></div>
-        <div className="footer__item"></div>
+        <div className="footer__segment">
+          <p className="footer__heading">Kontakt</p>
+          <div className="footer__item">
+            <p>Telefon:</p>
+            <p>906 59 303</p>
+          </div>
+          <div className="footer__item">
+            <p>Epost:</p>
+            <p>hugostoresund@gmail.com</p>
+          </div>
+        </div>
+        <div className="footer__segment">
+          <p className="footer__heading">Org.nr.</p>
+          <p>977314194</p>
+        </div>
+        <div className="footer__segment">
+          <p className="footer__heading">Adresse</p>
+          <p>Hugo Storesund</p>
+          <p>Lyngvegen 4</p>
+          <p>5382 Skogsvåg</p>
+        </div>
+        <div className="footer__segment">
+          <p className="footer__heading">OSS</p>
+          <p>Ofte stilte spørsmål</p>
+        </div>
+        <div className="footer__segment">
+          <p className="footer__heading">Minigraver-utleie</p>
+          <p>minigraveren.no</p>
+        </div>
       </div>
     </footer>
   );
