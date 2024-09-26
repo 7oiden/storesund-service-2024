816f65d51a6b89e2ee6bcc8565f837d78aa3fa45
Tommy Johnsen <7oiden@gmail.com>
2024-09-26 10:58:53 +0200

    footer complete

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,51 +6,56 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <div className="footer__segment">
-          <p className="footer__heading">Kontakt</p>
-          <div>
-            <div className="footer__item">
+        <div className="footer__segment footer__segment-1">
+          <div className="footer__contact">
+            <p className="footer__heading">Kontakt</p>
+            <div className="footer__line">
               <p>Telefon:</p>
               <FooterLink to="tel +4790659303">906 59 303</FooterLink>
             </div>
-            <div className="footer__item">
+            <div className="footer__line">
               <p>E-post:</p>
               <FooterLink to="mailto: hugostoresund@gmail.com">
-                hugostoresund@gmail.com
+                hugo.storesund@gmail.com
               </FooterLink>
             </div>
+            <LinkButton to="contact">Kontaktskjema</LinkButton>
           </div>
-          <LinkButton to="contact">Kontaktskjema</LinkButton>
         </div>
-        <div className="footer__segment">
-          <p className="footer__heading">Org.nr.</p>
-          <FooterLink to="https://data.brreg.no/enhetsregisteret/oppslag/enheter/977314194">
-            977314194
-          </FooterLink>
-        </div>
-        <div className="footer__segment">
-          <p className="footer__heading">Adresse</p>
+        <div className="footer__segment footer__segment-2">
           <div>
-            <p>Lyngvegen 4</p>
-            <p>5382 Skogsvåg</p>
+            <p className="footer__heading">Org.nr.</p>
+            <FooterLink to="https://data.brreg.no/enhetsregisteret/oppslag/enheter/977314194">
+              977314194
+            </FooterLink>
+          </div>
+          <div>
+            <p className="footer__heading">Adresse</p>
+            <div>
+              <p>Lyngvegen 4</p>
+              <p>5382 Skogsvåg</p>
+            </div>
           </div>
         </div>
-        <hr className="footer__divider"/>
-        <div className="footer__segment">
-          <p className="footer__heading">OSS</p>
-          <FooterLink to="contact">Ofte stilte spørsmål</FooterLink>
-        </div>
-        <div className="footer__segment">
-          <p className="footer__heading">Nøytral info om varmepumper</p>
-          <FooterLink to="https://www.varmepumpeinfo.no">
-            varmepumpeinfo.no
-          </FooterLink>
-        </div>
-        <div className="footer__segment">
-          <p className="footer__heading">Minigraver-utleie</p>
-          <FooterLink to="http://www.minigraveren.com">
-            minigraveren.no
-          </FooterLink>
+        <div className="footer__ver-divider footer__segment-3"></div>
+        <div className="footer__segment footer__segment-4">
+          <hr className="footer__hor-divider" />
+          <div>
+            <p className="footer__heading">OSS</p>
+            <FooterLink to="contact">Ofte stilte spørsmål</FooterLink>
+          </div>
+          <div>
+            <p className="footer__heading">Nøytral info om varmepumper</p>
+            <FooterLink to="https://www.varmepumpeinfo.no">
+              varmepumpeinfo.no
+            </FooterLink>
+          </div>
+          <div>
+            <p className="footer__heading">Minigraver-utleie</p>
+            <FooterLink to="http://www.minigraveren.com">
+              minigraveren.no
+            </FooterLink>
+          </div>
         </div>
       </div>
       <div className="footer__copyright">
